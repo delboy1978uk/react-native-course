e4e7d1109f8c896a819b2493f1328df6c5a61d55
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-07-21 17:22:25 +0200

    creating views with dimensions

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {StatusBar} from 'expo-status-bar';
 import {
     Alert,
+    Dimensions,
     StyleSheet,
     Text,
     SafeAreaView,
@@ -9,34 +10,22 @@ import {
     TouchableOpacity,
     TouchableHighlight,
     Button,
-    Platform
+    Platform,
+    View
 } from 'react-native';
 
 export default function App() {
-    const handlePress = () => alert('you clicked on shit!');
-    const handleImagePress = () => alert('you clicked on shit!');
+    console.log(Dimensions.get('screen'))
 
     return (
         <SafeAreaView style={[styles.container, customStyle]}>
-            <Text numberOfLines={2} onPress={handlePress}>AYE! how fucking awesome is this?! This is a long long long
-                piece of text, many many many many many many many many many many many many many hunners infinite
-                broomsticks long.</Text>
-            <Image source={require('./assets/favicon.png')}/>
-            <TouchableHighlight  onPress={handleImagePress} >
-                <Image source={{
-                    uri: 'https://picsum.photos/200/300',
-                    width: 200,
-                    height: 300
-                }}/>
-            </TouchableHighlight>
-            <Button
-                color="orange"
-                title={"Click Me!"}
-                onPress={() => Alert.alert('Incredible news', 'button tapped! Would you like to learn more?', [
-                    {text: 'yes', onPress: () => Alert.prompt('We want your details', 'we probably wont sell them', text => alert(text))},
-                    {text: 'no', onPress: () => alert('Naw')}
-                ])}
-            />
+            <View style={{
+                backgroundColor: 'dodgerblue',
+                width: '50%',
+                height: 70
+            }}>
+
+            </View>
         </SafeAreaView>
     );
 }
@@ -46,8 +35,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
         padding: Platform.OS === "android" ? StatusBar.currentHeight : 0
     },
 });

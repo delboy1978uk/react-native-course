bc0bb268dcb0376f23e26474b4a7cc412054c839
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-07-23 01:22:33 +0200

    custom text component with style

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Text, View} from "react-native";
+
+import AppText from './app/components/AppText';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-import {Text, View} from "react-native";
 
 export default function App() {
     return (
@@ -10,15 +12,7 @@ export default function App() {
             justifyContent: "center",
             alignItems: "center"
         }}>
-            <Text style={{
-                fontSize: 30,
-                fontStyle: "italic",
-                fontWeight: "600",
-                textTransform: "capitalize",
-                textDecorationLine: "underline",
-                textAlign: "center",
-                lineHeight: 50
-            }}>This is fucking awesome! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam culpa ea nostrum, nulla, odit pariatur quaerat, quis quos repellendus temporibus. Aspernatur at consectetur, dolor magnam repellat sit ut.</Text>
+            <AppText>This is fucking awesome! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam culpa ea nostrum, nulla, odit pariatur quaerat, quis quos repellendus temporibus. Aspernatur at consectetur, dolor magnam repellat sit ut.</AppText>
         </View>
     );
 }

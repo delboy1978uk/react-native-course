5239d45932a4aee5c372d0efad6d639cd7a78e27
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-07-23 00:13:44 +0200

    css type stuff (circles)

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import {View} from "react-native";
 
 export default function App() {
     return (
-        <WelcomeScreen />
+        <View style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <View style={{
+                backgroundColor: "dodgerblue",
+                height: 100,
+                width: 100,
+                borderWidth: 10,
+                borderColor: "royalblue",
+                borderRadius:  50
+            }}>
+
+            </View>
+        </View>
     );
 }

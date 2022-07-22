8b4bd6b221d156ead0958f3a40f84f13cf836349
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-07-23 00:57:04 +0200

    padding and margin

--- App.js
+++ App.js
@@ -21,9 +21,22 @@ export default function App() {
                 },
                 shadowOpacity: 1,
                 elevation: 20,
+                padding: 20,
+                paddingHorizontal: 10,
+                paddingLeft: 30,
             }}>
-
+                <View style={{
+                    backgroundColor: "gold",
+                    width: 50,
+                    height: 50,
+                }}></View>
             </View>
+            <View style={{
+                backgroundColor: "tomato",
+                width: 100,
+                height: 100,
+                margin: 20,
+            }} ></View>
         </View>
     );
 }

c816641dcd89b16100fb9fb10aeb6174dac6d47c
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-07-23 01:27:58 +0200

    adding icons

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 import AppText from './app/components/AppText';
 import ViewImageScreen from './app/screens/ViewImageScreen';
@@ -12,6 +13,7 @@ export default function App() {
             justifyContent: "center",
             alignItems: "center"
         }}>
+            <MaterialCommunityIcons name="email" size="200" color="dodgerblue"/>
             <AppText>This is fucking awesome! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam culpa ea nostrum, nulla, odit pariatur quaerat, quis quos repellendus temporibus. Aspernatur at consectetur, dolor magnam repellat sit ut.</AppText>
         </View>
     );

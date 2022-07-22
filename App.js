621aac348e47bd6a8365ccd33b41901ee0815e72
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-07-23 01:07:59 +0200

    text styling

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-import {View} from "react-native";
+import {Text, View} from "react-native";
 
 export default function App() {
     return (
@@ -10,33 +10,15 @@ export default function App() {
             justifyContent: "center",
             alignItems: "center"
         }}>
-            <View style={{
-                backgroundColor: "dodgerblue",
-                height: 100,
-                width: 100,
-                shadowColor: "grey",
-                shadowOffset: {
-                    width: 10,
-                    height: 10,
-                },
-                shadowOpacity: 1,
-                elevation: 20,
-                padding: 20,
-                paddingHorizontal: 10,
-                paddingLeft: 30,
-            }}>
-                <View style={{
-                    backgroundColor: "gold",
-                    width: 50,
-                    height: 50,
-                }}></View>
-            </View>
-            <View style={{
-                backgroundColor: "tomato",
-                width: 100,
-                height: 100,
-                margin: 20,
-            }} ></View>
+            <Text style={{
+                fontSize: 30,
+                fontStyle: "italic",
+                fontWeight: "600",
+                textTransform: "capitalize",
+                textDecorationLine: "underline",
+                textAlign: "center",
+                lineHeight: 50
+            }}>This is fucking awesome! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam culpa ea nostrum, nulla, odit pariatur quaerat, quis quos repellendus temporibus. Aspernatur at consectetur, dolor magnam repellat sit ut.</Text>
         </View>
     );
 }

b2a6e2f28a555b28509c2173a4848c380607cef8
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-07-23 14:41:37 +0200

    use index.js so as not to need './components/Apptext/AppText'

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text, View} from "react-native";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 
-import AppText from './app/components/AppText/AppText';
+import AppText from './app/components/AppText';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 

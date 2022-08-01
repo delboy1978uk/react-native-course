2d160b98e7d1c5cbc5af00c483749de616ba9536
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-01 09:40:09 +0200

    text input

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import {Text, View} from "react-native";
+import React, {useState} from 'react';
+import {Text, TextInput, View} from "react-native";
 
-import ListingsScreen from './app/screens/ListingsScreen';
+import Screen from './app/components/Screen';
 
 export default function App() {
+    const [firstname, setFirstname] = useState('');
+
     return (
-        <ListingsScreen />
+        <Screen>
+            <Text>{firstname}</Text>
+            <TextInput
+                secureTextEntry
+                clearButtonMode="always"
+                maxLength={20}
+                onChangeText={text => setFirstname(text)}
+                placeholder="First Name"
+                style={{
+                    borderBottomColor: '#ccc',
+                    borderBottomWidth: 1,
+                }}
+            />
+        </Screen>
     );
 }

cf1086ef48c0ea5cc1d31ea90127041392870cab
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-04 23:49:27 +0200

    switch component

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
-import {Text, TextInput, View} from "react-native";
+import {Switch, Text, TextInput, View} from "react-native";
 
 import AppTextInput from './app/components/AppTextInput';
 import Screen from './app/components/Screen';
 
 export default function App() {
-    const [firstname, setFirstname] = useState('');
+    const [isNew, setIsNew] = useState(false);
 
     return (
         <Screen>
-            <AppTextInput icon="email" placeholder="Email address"/>
+            <Switch value={isNew} onValueChange={ newValue => setIsNew(newValue) } />
         </Screen>
     );
 }

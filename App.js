10e21834f17e46d247612723711b99ab8c29ab60
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-11 22:12:21 +0200

    request camera permissions

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Switch, Text, TextInput, View} from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 
-import ListingEditScreen from './app/screens/ListingEditScreen'
+import Screen from './app/components/Screen'
 
 export default function App() {
+    const requestPermission = async () => {
+        const {granted} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (!granted) {
+            alert('What?! No! You need to click on allow always!');
+        }
+    }
+
+    useEffect(() => {
+        requestPermission();
+    }, []);
+
     return (
-        <ListingEditScreen />
+        <Screen></Screen>
     )
 }

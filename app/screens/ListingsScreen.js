f49b45600eefca3c90a95215c0d91a7301a020f3
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-20 02:49:58 +0200

    check api connectivity

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -2,14 +2,17 @@ import React, {useEffect, useState} from 'react';
 
 import Card from '../components/Card'
 import Screen from '../components/Screen'
-import {FlatList, StyleSheet, TouchableWithoutFeedback} from "react-native";
+import {FlatList, StyleSheet, TouchableWithoutFeedback, View} from "react-native";
 
 import listingsApi from '../api/listings'
 import colors from '../config/colors'
+import Button from '../components/Button'
+import Text from '../components/Text'
 import routes from '../navigation/routes'
 
 function ListingsScreen({navigation}) {
 
+    const [error, setError] = useState(false);
     const [listings, setListings] = useState([]);
 
     useEffect(() => {
@@ -18,11 +21,25 @@ function ListingsScreen({navigation}) {
 
     const loadListings = async () => {
         const response = await listingsApi.getListings();
+
+        if (!response.ok) {
+            setError(true);
+
+            return;
+        }
+
+        setError(false);
         setListings(response.data);
     }
 
     return (
         <Screen style={styles.screen}>
+            { error &&
+                <View>
+                    <Text>Could not retrieve the listings</Text>
+                    <Button textColor={'white'} color={'primary'} title={'retry'} onPress={ loadListings }/>
+                </View>
+            }
             <FlatList
                 data={listings}
                 keyExtractor={listing => listing.id}

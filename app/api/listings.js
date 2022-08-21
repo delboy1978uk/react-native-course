f4f91ac23d578421656bf422c02845ceb507e7c9
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-21 16:44:58 +0200

    add post listings endpoint

--- app/api/listings.js
+++ app/api/listings.js
@@ -2,6 +2,7 @@ import apiClient from './client'
 
 const endpoint = '/listings';
 const getListings = () => apiClient.get(endpoint);
+const postListings = data => apiClient.post(endpoint, data);
 
 export default {
     getListings,

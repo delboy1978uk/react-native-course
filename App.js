b5be50fd58fc7f126c7444e8d07d1b3600d095f3
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-27 07:58:45 +0200

    button status changimng dependent on internet connectioon status

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
-import NetInfo from '@react-native-community/netinfo';
+import NetInfo, {useNetInfo} from '@react-native-community/netinfo';
 
 import AppNavigator from "./app/navigation/AppNavigator"
+import Button from "./app/components/Button"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import navigationTheme from "./app/navigation/NavigationTheme"
 
 export default function App() {
-    const unsubscribe = NetInfo.addEventListener(netInfo => console.log(netInfo));
-    unsubscribe();
+    const netInfo = useNetInfo();
 
+    return <Button disabled={netInfo.isInternetReachable} title={netInfo.isInternetReachable ? 'TRUE' : 'FALSE'}/>
 
-    return(
-        <NavigationContainer theme={navigationTheme}>
-            <AppNavigator />
-        </NavigationContainer>
-    )
+    // return(
+    //     <NavigationContainer theme={navigationTheme}>
+    //         <AppNavigator />
+    //     </NavigationContainer>
+    // )
 }

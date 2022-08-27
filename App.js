bcee0b07f8bd98b49d94b03f5f846eba6dd18844
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-27 10:06:16 +0200

    save and fetch from async storage

--- App.js
+++ App.js
@@ -7,11 +7,23 @@ import AppNavigator from "./app/navigation/AppNavigator"
 import Button from "./app/components/Button"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import navigationTheme from "./app/navigation/NavigationTheme"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-    const netInfo = useNetInfo();
+    const demo = async () => {
+        try {
+            await AsyncStorage.setItem('person', JSON.stringify({id: 1}));
+            const value = await AsyncStorage.getItem('person');
+            const person = JSON.parse(value);
+            console.log(person);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    return <Button disabled={netInfo.isInternetReachable} title={netInfo.isInternetReachable ? 'TRUE' : 'FALSE'}/>
+    demo();
+
+    return null;
 
     // return(
     //     <NavigationContainer theme={navigationTheme}>

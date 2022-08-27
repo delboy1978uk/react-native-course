cbc9059b7b261a548a97e915e62e7870c47f5d56
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-27 07:42:22 +0200

    get netinfo and log

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
+import NetInfo from '@react-native-community/netinfo';
 
 import AppNavigator from "./app/navigation/AppNavigator"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import navigationTheme from "./app/navigation/NavigationTheme"
 
 export default function App() {
-    return(
+    NetInfo.fetch().then(netInfo => console.log(netInfo));
 
+    return(
         <NavigationContainer theme={navigationTheme}>
             <AppNavigator />
         </NavigationContainer>

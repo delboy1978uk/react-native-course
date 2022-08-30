da97b09bfcc40eccc436d75d2ffe9a8eaa202b3c
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-08-30 12:01:46 +0200

    adding cache layer WIP

--- App.js
+++ App.js
@@ -10,24 +10,24 @@ import navigationTheme from "./app/navigation/NavigationTheme"
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-    const demo = async () => {
-        try {
-            await AsyncStorage.setItem('person', JSON.stringify({id: 1}));
-            const value = await AsyncStorage.getItem('person');
-            const person = JSON.parse(value);
-            console.log(person);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    // const demo = async () => {
+    //     try {
+    //         await AsyncStorage.setItem('person', JSON.stringify({id: 1}));
+    //         const value = await AsyncStorage.getItem('person');
+    //         const person = JSON.parse(value);
+    //         console.log(person);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+    //
+    // demo();
+    //
+    // return null;
 
-    demo();
-
-    return null;
-
-    // return(
-    //     <NavigationContainer theme={navigationTheme}>
-    //         <AppNavigator />
-    //     </NavigationContainer>
-    // )
+    return(
+        <NavigationContainer theme={navigationTheme}>
+            <AppNavigator />
+        </NavigationContainer>
+    )
 }

0433008b44f071f6d973f3b25a5f2a184aee8362
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-09-09 01:24:34 +0200

    offline notice

--- App.js
+++ App.js
@@ -1,33 +1,25 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
 import NetInfo, {useNetInfo} from '@react-native-community/netinfo';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import AppNavigator from "./app/navigation/AppNavigator"
 import Button from "./app/components/Button"
+import OfflineNotice from "./app/components/OfflineNotice"
+import Text from "./app/components/Text"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import navigationTheme from "./app/navigation/NavigationTheme"
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-    // const demo = async () => {
-    //     try {
-    //         await AsyncStorage.setItem('person', JSON.stringify({id: 1}));
-    //         const value = await AsyncStorage.getItem('person');
-    //         const person = JSON.parse(value);
-    //         console.log(person);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-    //
-    // demo();
-    //
-    // return null;
 
     return(
-        <NavigationContainer theme={navigationTheme}>
-            <AppNavigator />
-        </NavigationContainer>
+        <View style={{flex:1}}>
+            <OfflineNotice />
+            <NavigationContainer theme={navigationTheme}>
+                <AppNavigator />
+            </NavigationContainer>
+        </View>
     )
 }
---
+++ app/components/OfflineNotice.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {StyleSheet, View} from "react-native";
+import {useNetInfo} from "@react-native-community/netinfo";
+
+import colors from "../config/colors";
+import Text from "./Text";
+
+function OfflineNotice(props) {
+    const netInfo = useNetInfo();
+    console.log(netInfo);
+
+    if (netInfo.type !== 'unkown' && netInfo.isInternetReachable === false) {
+        return(
+            <View style={styles.container}>
+                <Text style={styles.text}>No Internet connection</Text>
+            </View>
+        );
+    }
+
+    return null;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.primary,
+        color: colors.white,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        paddingTop: 20
+    },
+    text: {
+        color: colors.white,
+    }
+})
+
+export default OfflineNotice;
